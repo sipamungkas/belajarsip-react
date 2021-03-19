7daa79242b86c9241e06dcd913dcdb3555bc8868
Ragil Burhanudin Pamungkas <om.ragil7@gmail.com>
2021-03-19 20:50:00 +0700

    fix: responsive on 1336px on reset password

--- src/components/ResetRightSide.js
+++ src/components/ResetRightSide.js
@@ -3,26 +3,6 @@ import PropTypes from "prop-types";
 
 function ResetRightSide(props) {
   const { step } = props;
-  if (step === "1") {
-    return (
-      <div className="right-side">
-        <h1>Reset Password</h1>
-        <div>
-          <h2>Enter your email address linked to this account.</h2>
-          <h3>We will send you the verification code to reset your password</h3>
-        </div>
-        <div>
-          <div className="form-container">
-            <input type="text" id="username" placeholder="Username or Email" />
-            <label htmlFor="username">Username or Email</label>
-          </div>
-          <div className="btn-container">
-            <div className="btn-login">Send</div>
-          </div>
-        </div>
-      </div>
-    );
-  }
   if (step === "2") {
     return (
       <div className="right-side">
@@ -69,6 +49,24 @@ function ResetRightSide(props) {
       </div>
     );
   }
+  return (
+    <div className="right-side">
+      <h1>Reset Password</h1>
+      <div>
+        <h2>Enter your email address linked to this account.</h2>
+        <h3>We will send you the verification code to reset your password</h3>
+      </div>
+      <div>
+        <div className="form-container">
+          <input type="text" id="username" placeholder="Username or Email" />
+          <label htmlFor="username">Username or Email</label>
+        </div>
+        <div className="btn-container">
+          <div className="btn-login">Send</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 ResetRightSide.propTypes = {
