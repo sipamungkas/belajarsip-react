e5147c741f9be9beba5b6c7865a19f5487a7ad3d
Ragil Burhanudin Pamungkas <om.ragil7@gmail.com>
2021-04-05 00:06:30 +0700

    refactor: change create to new task on fasilitator

--- src/components/DashboardScheduleList.js
+++ src/components/DashboardScheduleList.js
@@ -66,7 +66,7 @@ export default function DashboardScheduleList(props) {
               alt="add"
               className="add-task-icon"
             />
-            Create
+            New Task
           </button>
         </div>
       </section>
